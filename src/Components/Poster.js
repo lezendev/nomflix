6c17573327ac25e68cc04812146184f04236ab76
lezendev <lezendev@gmail.com>
2020-08-11 18:20:56 +0900

    copy

--- src/Components/Poster.js
+++ src/Components/Poster.js
@@ -2,7 +2,7 @@ import React from "react";
 import Proptypes from "prop-types";
 import styled from "styled-components";
 import {Link} from "react-router-dom";
-import noPoster from "assets/noPosterSmall.png";
+import noPoster from "./noPosterSmall.png";
 
 
 const Container = styled.div`
@@ -60,7 +60,7 @@ const Poster = ({ id, imageUrl, title, rating, year, isMovie = false }) =>
         <Container>
             <ImageContainer>
                 <Image
-                    bgUrl={imageUrl ? `https://image.tmdb.org/t/p/w300${imageUrl}` : {noPoster}}/>
+                    bgUrl={imageUrl ? `https://image.tmdb.org/t/p/w300${imageUrl}` : require("./noPosterSmall.png")}/>
                 <Rating>
                 <span role="img" aria-label="rating">
                     ☆
