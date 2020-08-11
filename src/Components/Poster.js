bb1d8032f0381c766d04b95564265dbe896f6609
lezendev <lezendev@gmail.com>
2020-08-11 18:39:29 +0900

    script

--- src/Components/Poster.js
+++ src/Components/Poster.js
@@ -2,7 +2,6 @@ import React from "react";
 import Proptypes from "prop-types";
 import styled from "styled-components";
 import {Link} from "react-router-dom";
-import noPoster from "./noPosterSmall.png";
 
 
 const Container = styled.div`
